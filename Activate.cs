8ee213dbea021308534c3619fc160b9cb75def48
AhmedAshry7 <121507692+AhmedAshry7@users.noreply.github.com>
2024-01-26 18:34:42 +0200

    Add files via upload

---
+++ Activate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBapplication
+{
+    public partial class Activate : Form
+    {
+        Controller controllerObj;
+        public Activate()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Activate_Load(object sender, EventArgs e)
+        {
+            controllerObj = new Controller();
+            comboBox1.DataSource = controllerObj.get0subs();
+            comboBox1.DisplayMember = "SubscriptionID";
+            comboBox1.ValueMember = "SubscriptionID";
+            //comment
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int f = controllerObj.activatesub(Convert.ToInt32(comboBox1.SelectedValue));
+            if (f != 0)
+            {
+                MessageBox.Show("activated");
+                comboBox1.DataSource = controllerObj.get0subs();
+                comboBox1.DisplayMember = "SubscriptionID";
+                comboBox1.ValueMember = "SubscriptionID";
+            }
+        }
+    }
+}
